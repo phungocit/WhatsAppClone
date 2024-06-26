5c543f774f3a2f4f6192a42c1bc94063147f881b
Foo Tran <tranngocphu2904@gmail.com>
2024-06-26 22:52:47 +0700

    #feat: Version 2

---
+++ WhatsAppClone/Scenes/Chat/Views/BubbleTextView.swift
@@ -0,0 +1,58 @@
+//
+//  BubbleTextView.swift
+//  WhatsAppClone
+//
+//  Created by Phil Tran on 3/14/24.
+//
+
+import SwiftUI
+
+struct BubbleTextView: View {
+    let item: MessageItem
+
+    var body: some View {
+        HStack(alignment: .bottom, spacing: 5) {
+            if item.showGroupPartnerInfo {
+                CircularProfileImageView(item.sender?.profileImageUrl, size: .mini)
+            }
+
+            if item.direction == .sent {
+                timeStampTextView
+            }
+
+            Text(item.text)
+                .padding(10)
+                .background(item.backgroundColor)
+                .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                .applyTail(item.direction)
+                .contextMenu {
+                    Button {} label: {
+                        Label("ContextMenu", systemImage: "heart")
+                    }
+                }
+
+            if item.direction == .received {
+                timeStampTextView
+            }
+        }
+        .shadow(color: Color(.systemGray3).opacity(0.1), radius: 5, x: 0, y: 20)
+        .frame(maxWidth: .infinity, alignment: item.alignment)
+        .padding(.leading, item.leadingPadding)
+        .padding(.trailing, item.trailingPadding)
+    }
+
+    private var timeStampTextView: some View {
+        Text(item.timeStamp.formatToTime)
+            .font(.footnote)
+            .foregroundStyle(.gray)
+    }
+}
+
+#Preview {
+    ScrollView {
+        BubbleTextView(item: .sentPlaceholder)
+        BubbleTextView(item: .receivedPlaceholder)
+    }
+    .frame(maxWidth: .infinity)
+    .background(Color.gray.opacity(0.4))
+}
